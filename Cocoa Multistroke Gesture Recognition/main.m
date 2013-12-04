01eb599f9d91851e9c05986aa59228b8cc326834
Mathew Huusko V <mhuusko5@gmail.com>
2013-12-04 17:51:36 -0500

    Update @property use, ARC and modern syntax, and beautify code

--- Cocoa Multistroke Gesture Recognition/main.m
+++ Cocoa Multistroke Gesture Recognition/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Cocoa Multistroke Gesture Recognition
-//
-//  Created by Mathew Huusko V on 9/14/13.
-//  Copyright (c) 2013 Mathew Huusko V. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[]) {
